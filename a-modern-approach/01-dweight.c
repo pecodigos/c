fbe78cd592132f98475f07574eb2eaf414b605e7
pecodigos <pedrenriquecunha@gmail.com>
2024-09-30 00:33:21 -0300

    Refactoring dweight

--- a-modern-approach/01-dweight.c
+++ a-modern-approach/01-dweight.c
@@ -3,12 +3,10 @@
 // Computes the dimensional weight of a 12" x 10" x 8" box
 
 int main(void) {
-    int volume, weight;
-
     int height = 8;
     int lenght = 12;
     int width = 10;
-    volume = height * lenght * width;
+    int volume = height * lenght * width;
     
     printf("Dimensions: %dx%dx%d\n", lenght, width, height);
     printf("Volume (cubic inches): %d\n", volume);
