e85661c655a025d5f228a9d3aaf099f920b3776f
pecodigos <pedrenriquecunha@gmail.com>
2024-09-29 23:56:57 -0300

    Easy exercise

---
+++ a-modern-approach/04-parkinson.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// directives
+
+int main(void) {
+    // statements
+    printf("Parkinson's Law:\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+
+    return 0;
+}
+/* Output:
+Parkisons's Law:
+Work expands so as to fill the time
+available for its completion.
+*/
