c1dc50f99cf2c23aa6b0fc2a3a101437416fff2b
pecodigos <pedrenriquecunha@gmail.com>
2024-10-07 00:44:26 -0300

    results are now consts

--- a-modern-approach/addfrac.c
+++ a-modern-approach/addfrac.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    int num1, denom1, num2, denom2, result_num, result_denom;
+    int num1, denom1, num2, denom2;
 
     printf("Enter first fraction: ");
     scanf("%d/%d", &num1, &denom1);
@@ -9,10 +9,10 @@ int main(void) {
     printf("Enter second fraction: ");
     scanf("%d/%d", &num2, &denom2);
 
-    result_num = num1 * denom2 + num2 * denom1;
-    result_denom = denom1 * denom2;
+    const int result_num = num1 * denom2 + num2 * denom1;
+    const int result_denom = denom1 * denom2;
 
     printf("The sum is %d/%d\n", result_num, result_denom);
-    
+
     return 0;
 }
